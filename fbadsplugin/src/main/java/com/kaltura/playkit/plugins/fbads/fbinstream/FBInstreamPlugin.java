c435de878d044a744ac367c23f7c99df0d28c5ba
Gourav Saxena <gourav.saxena@kaltura.com>
2020-11-11 19:48:52 +0530

    - Travis build

--- fbadsplugin/src/main/java/com/kaltura/playkit/plugins/fbads/fbinstream/FBInstreamPlugin.java
+++ fbadsplugin/src/main/java/com/kaltura/playkit/plugins/fbads/fbinstream/FBInstreamPlugin.java
@@ -471,7 +471,6 @@ public class FBInstreamPlugin extends PKPlugin implements AdsProvider {
 
     private void createAdInfo(FBInStreamAdBreak adBreak, FBInStreamAd ad) {
 
-        
         String adDescription = ad.getAdPlacementId();
         long adDuration = ad.getAdBreakTime() * Consts.MILLISECONDS_MULTIPLIER;
         long adPlayHead = getCurrentPosition() * Consts.MILLISECONDS_MULTIPLIER;
